ede301a394420823dfcecfa35366c22eab3f99d7
Lucas Silva <helloiamlucassilva@gmail.com>
2024-10-29 17:44:03 -0300

    aplicando react hook form

--- src/components/NewTransactionModal/styles.ts
+++ src/components/NewTransactionModal/styles.ts
@@ -49,10 +49,16 @@ export const Content = styled(Dialog.Content)`
       padding: 0 1.25rem;
       border-radius: 6px;
       margin-top: 1.5rem;
+      cursor: pointer;
 
       cursor: pointer;
 
-      &:hover {
+      &:disabled {
+        opacity: 0.3;
+        cursor: not-allowed;
+      }
+
+      &:not(:disabled):hover {
         background-color: ${props => props.theme['green-700']};
         transition: background-color 0.2s;
       }
